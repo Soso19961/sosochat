67c2e5c274df601de4ae9ed3638b7b00e053ec01
Anna Sabirova <annasabirova@MacBook-Air-Anna.local>
2023-08-13 11:40:30 +0800

    init

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-  docker build -t botden1 .
+	docker build -t botden1 .
 
 run:
-  docker run -d -p 3000:3000 --name botden1 --rm botden1
+	docker run -d -p 3000:3000 --name botden1 --rm botden1
--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import { openai } from './openai.js'
 console.log(config.get('TEST_ENV'))
 
 const INITIAL_SESSION = {
-    messages: [ ],
+    messages: [],
 }
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
@@ -24,28 +24,41 @@ bot.command('new', async (ctx) => {
 bot.on(message('voice'), async ctx => {
     ctx.session ??= INITIAL_SESSION
     try {
-        await ctx.reply(code('АлешкинGPT думает, ожидайте...'))
-        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
-        const userId = String(ctx.message.from.id)
-        const oggPath = await ogg.create(link.href, userId)
-        const mp3Path = await ogg.toMp3(oggPath, userId)
+        await ctx.reply("<b><i>АлешкинGPT думает, ожидайте...</i></b>", { parse_mode: "HTML" });
 
-        const text = await openai.transcription(mp3Path)
-        await ctx.reply(code(`Ваш запрос: ${text}`))
 
-        ctx.session.messages.push({ role: openai.roles.USER, content: text })
-        
+        console.log("Getting file link...");
+        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+        console.log("File link obtained:", link.href);
+    
+        console.log("Creating ogg...");
+        const userId = String(ctx.message.from.id);
+        const oggPath = await ogg.create(link.href, userId);
+        console.log("Ogg created:", oggPath);
+    
+        console.log("Converting ogg to mp3...");
+        const mp3Path = await ogg.toMp3(oggPath, userId);
+        console.log("Ogg converted to mp3:", mp3Path);
+
+        console.log("Transcribing...");
+        const text = await openai.transcription(mp3Path);
+        await ctx.reply("<i>" + text + "</i>", { parse_mode: "HTML" });
+
+
+        console.log("Chatting with OpenAI...");
+        ctx.session.messages.push({ role: openai.roles.USER, content: text });
         const response = await openai.chat(ctx.session.messages);
+        //console.log("OpenAI chat done:", { content: response.content });
+
 
         ctx.session.messages.push({
              role: openai.roles.ASSISTANT,
               content: response.content,
-             })
+        });
 
-
-        await ctx.reply(response.content)
+        await ctx.reply(response.content);
     } catch (e) {
-        console.log('Error while voice message', e.message)
+        console.log('Error while voice message:', e.message, '\nFull Error:', e);
     } 
 })
 
@@ -54,26 +67,23 @@ bot.on(message('text'), async ctx => {
     try {
         await ctx.reply(code('АлешкинGPT думает, ожидайте...'))
 
-
-
-        ctx.session.messages.push({ role: openai.roles.USER, content: ctx.message.text })
+        ctx.session.messages.push({ role: openai.roles.USER, content: ctx.message.text });
         
-        const response = await openai.chat(ctx.session.messages)
+        const response = await openai.chat(ctx.session.messages);
 
         ctx.session.messages.push({
              role: openai.roles.ASSISTANT,
               content: response.content,
-             })
+        });
 
-
-        await ctx.reply(response.content)
+        await ctx.reply(response.content);
     } catch (e) {
-        console.log('Error while voice message', e.message)
+        console.log('Error while text message:', e.message, '\nFull Error:', e);
     } 
 })
 
 bot.command('start', async (ctx) => {
-    await ctx.reply("Добро пожаловать! Я готов ответить на ваши голосовые и текстовые вопросы. Просто начните диалог, отправив сообщение или нажав на иконку микрофона.")
+    await ctx.reply("<b>Добро пожаловать! Я готов ответить на ваши голосовые и текстовые вопросы. Просто начните диалог, отправив сообщение или нажав на иконку микрофона.</b>", { parse_mode: "HTML" });
 })
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
